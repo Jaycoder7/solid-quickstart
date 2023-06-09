cc794fe2918a83107229a9e5d841d888ede991b4
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-06-09 14:42:54 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>The Entrepreneurial Hour Zoom</Title>
+      <h1>The Entrepreneurial Hour</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        We meet every Wednesday at 8 AM. You can attend in person at Tech Alpharetta • 2972 Webb Bridge Road, Alpharetta, GA 30009{"\n"}
+        OR
+        <a href="https://us06web.zoom.us/j/99801555269" target="_blank">
+        Attend meeting using this zoom link{"\n"} 
+        *This links is active at 7:45AM*
         </a>{" "}
         to learn how to build SolidStart apps.
       </p>

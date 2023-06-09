88cac5c43d8fc4f941f620a3bffa7f2b471aa883
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-06-09 16:24:44 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,29 @@ export default function Home() {
     <main>
       <Title>The Entrepreneurial Hour Zoom</Title>
       <h1>The Entrepreneurial Hour</h1>
-      <Counter />
+       <head>
+    <title>Title of the document</title>
+    <style>
+      .button {
+        display: inline-block;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        color: #ffffff;
+        background-color: #7aa8b7;
+        border-radius: 6px;
+        outline: none;
+        transition: 0.3s;
+      }
+      .button:hover {
+        background-color: #c2c7c7;
+      }
+    </style>
+  </head>
+  <body>
+    <a class="button" href="https://us06web.zoom.us/j/99801555269">Zoom</a>
+  </body>
+      
       <p>
         We meet every Wednesday at 8 AM. You can attend in person at Tech Alpharetta • 2972 Webb Bridge Road, Alpharetta, GA 30009
         OR

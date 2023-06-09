9d131896f05149aa67e648da2174426ba0caf948
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-06-09 16:22:42 -0400

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,9 +4,28 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button onclick="https://us06web.zoom.us/j/99801555269">
-      Zoom
-    </button>
+    <head>
+    <title>Title of the document</title>
+    <style>
+      .button {
+        display: inline-block;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        color: #ffffff;
+        background-color: #7aa8b7;
+        border-radius: 6px;
+        outline: none;
+        transition: 0.3s;
+      }
+      .button:hover {
+        background-color: #c2c7c7;
+      }
+    </style>
+  </head>
+  <body>
+    <a class="button" href="https://us06web.zoom.us/j/99801555269">Zoom</a>
+  </body>
 
   );
 }
